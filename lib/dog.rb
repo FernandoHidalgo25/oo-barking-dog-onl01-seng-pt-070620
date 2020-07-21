5c698d328d68a6d298ed0da42b56353df1fbe186
Fernando Hidalgo <fernandojesus.hidalgominaya9@gmail.com>
2020-07-21 04:05:37 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,18 +1,18 @@
 class Dog
  
-  def .name=(dogs_name)
+  def name=(dogs_name)
     @this_dogs_name = dogs_name
   end
  
-  def .name
+  def name
     @this_dogs_name
   end
   
-  def .bark=(dogs_bark)
+  def bark=(dogs_bark)
     @this_dogs_bark = dogs_bark
   end
   
-  def .bark
+  def bark
     @this_dogs_bark
   end 
 end
